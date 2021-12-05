51480f9abdc4974b2af357885ea27da707a69f9c
Hanikat <hanikat@kth.se>
2021-12-05 12:49:03 +0100

    Minor refactoring and cleanup of Program class

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -2,6 +2,7 @@
 using AdventOfCode2021.Answers.Problems;
 using AdventOfCode2021.Common;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode2021
 {
@@ -9,14 +10,41 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
+            // print program information
             Console.WriteLine("Welcome to Advent of Code 2021 problem solver.");
             Console.WriteLine("For a complete list of the problems targeted by this application, see: https://adventofcode.com/2021");
             Console.WriteLine("----- ----- ------");
 
+            // prompt the user for which problem to solve
+            var problems = Problem.EnumerateProblems<Problem>().ToList();
+            int problemNumberToSolve = GetSelectedProblem(problems);
+            Problem problemToSolve = problems[problemNumberToSolve];
 
+            // prompt user for which input source and input to use
+            bool useFileInput = FileInputIsUsed();
+            SetProblemInput(problemToSolve, useFileInput);
+
+            // solve the problem and time its execution
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            problemToSolve.Solve();
+            watch.Stop();
+
+            // print the result of program execution
+            Console.WriteLine($"The problem was solved in {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"The solution to the problem is: ${problemToSolve.GetAnswer()}");
+        }
+
+
+        /// <summary>
+        /// Prompts the user for which problem to solve
+        /// </summary>
+        /// <param name="problems">A ordered list of all available problems which the user can choose to solve.</param>
+        /// <returns>The index of the problem to solve</returns>
+        private static int GetSelectedProblem(IEnumerable<Problem> problems)
+        {
             Console.WriteLine("Please choose which problem to solve:");
 
-            var problems = Problem.EnumerateProblems<Problem>();
             byte i = 0;
 
             foreach (var problem in problems)
@@ -24,17 +52,16 @@ namespace AdventOfCode2021
                 Console.WriteLine($"{++i} - {problem.ProblemName}");
             }
 
-
             int selectedNumber;
             while (true)
             {
                 Console.Write("Enter the number of the problem to solve: ");
                 var input = Console.ReadLine();
-                
+
 
                 if (int.TryParse(input, out selectedNumber))
                 {
-                    if(selectedNumber > i || selectedNumber < 1)
+                    if (selectedNumber > i || selectedNumber < 1)
                     {
                         Console.WriteLine($"The selected number is out of bounds. Please specify a number between 1 and {i}.");
                     }
@@ -49,8 +76,17 @@ namespace AdventOfCode2021
                 }
             }
 
-            var problemToSolve = problems.ElementAt(selectedNumber - 1);
+            return selectedNumber - 1;
+        }
 
+
+        /// <summary>
+        /// Prompts the user for which input source to use for the problem
+        /// </summary>
+        /// <returns>returns true if file input is used, returns false if console input is used</returns>
+        private static bool FileInputIsUsed()
+        {
+            int selectedNumber;
             while (true)
             {
                 Console.WriteLine("Which input method would you like to use?");
@@ -75,7 +111,17 @@ namespace AdventOfCode2021
                 }
             }
 
-            if(selectedNumber == 1)
+            return selectedNumber == 1;
+        }
+
+        /// <summary>
+        /// Sets the input for the problem to solve.
+        /// </summary>
+        /// <param name="problemToSolve">The problem to be solved.</param>
+        /// <param name="useFileInput">Marks wether the input should be taken from file or console.</param>
+        private static void SetProblemInput(Problem problemToSolve, bool useFileInput)
+        {
+            if (useFileInput)
             {
                 problemToSolve.GetInputFromFile();
             }
@@ -85,15 +131,6 @@ namespace AdventOfCode2021
                 var input = Console.ReadLine();
                 problemToSolve.SetInput(input);
             }
-
-            var watch = new System.Diagnostics.Stopwatch();
-
-            watch.Start();
-            problemToSolve.Solve();
-            watch.Stop();
-
-            Console.WriteLine($"The problem was solved in {watch.ElapsedMilliseconds} ms");
-            Console.WriteLine($"The solution to the problem is: ${problemToSolve.GetAnswer()}");
         }
     }
 }
